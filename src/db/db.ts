460c97f222f6837d080b2ad410f9646d302af710
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-08-19 08:17:52 -0300

    feat: roda querys no banco de dados

--- src/db/db.ts
+++ src/db/db.ts
@@ -13,7 +13,7 @@ const dbConfig = {
   },
 };
 
-export async function connectToDatabase(): Promise<sql.ConnectionPool> {
+async function connectToDatabase(): Promise<sql.ConnectionPool> {
   try {
     const pool = await sql.connect(dbConfig);
     console.log("Conexão com o banco de dados estabelecida com sucesso.");
@@ -24,14 +24,24 @@ export async function connectToDatabase(): Promise<sql.ConnectionPool> {
   }
 }
 
-export async function disconnectFromDatabase(
-  pool: sql.ConnectionPool,
-): Promise<void> {
+async function disconnectFromDatabase(pool: sql.ConnectionPool): Promise<void> {
   try {
     await pool.close();
     console.log("Conexão com o banco de dados encerrada com sucesso.");
   } catch (error) {
     console.error("Erro ao desconectar do banco de dados:", error);
+    throw error;
+  }
+}
+
+export async function executeQuery(query: string): Promise<any> {
+  try {
+    const pool = await connectToDatabase();
+    const result = await pool.request().query(query);
+    await disconnectFromDatabase(pool);
+    return result.recordset;
+  } catch (error) {
+    console.error("Erro ao executar a consulta:", error);
     throw error;
   }
 }
