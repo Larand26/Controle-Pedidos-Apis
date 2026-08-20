8bce45dc511782d7cdc44a5bd0bc49c70f8cde4f
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-08-20 14:17:52 -0300

    build: ajusta o nome do app no pm2 para controle-pedidos-apis

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "api-local-gen",
+      name: "controle-pedidos-apis",
       script: "dist/server.js",
       instances: 1,
       exec_mode: "cluster",
